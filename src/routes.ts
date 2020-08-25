0f456d67fde0802af66b58857b727705287e5c36
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-24 21:34:10 -0300

    feat: adjust routes to enable wallet and auth routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import Router from 'koa-router';
+import Auth from './controllers/auth';
+import Wallet from './controllers/wallet';
 
 const router = new Router();
 
@@ -9,5 +11,12 @@ router.get('/', async (ctx, next) => {
     };
     await next();
 });
+
+router.get('/wallet/:id/statement', Wallet.statement);
+router.post('/wallet/:id/payment', Wallet.payment);
+router.post('/wallet/:id/withdraw', Wallet.withdraw);
+router.post('/wallet/:id/deposit', Wallet.deposit);
+router.post('/auth/sign_in', Auth.sign_in);
+router.post('/auth/sign_up', Auth.sign_up);
 
 export default router;
