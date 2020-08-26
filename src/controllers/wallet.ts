ed333d5a1e44deda942b0246f8c6da9183560c86
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 17:54:01 -0300

    feat: add wallet.statement controller

--- src/controllers/wallet.ts
+++ src/controllers/wallet.ts
@@ -1,4 +1,38 @@
 import { ParameterizedContext, Next } from 'koa';
+import Errors from '../utils/errors';
+/**
+ * Controller responsável pela lógica relativa a obtenção de Extratos da Conta Corrente;
+ * @param ctx
+ * @param next
+ */
+const statement = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
+    const { page_size = 15, page_number = 1 } = ctx.request.query;
+    const wallet_id = ctx.state.wallet_id;
+
+    const wallet = await Wallet.get_wallet(wallet_id);
+
+    if (!wallet) {
+        ctx.status = 401;
+        ctx.body = {
+            status: 'error',
+            data: {
+                message: Errors.UNAUTHORIZED_ERROR,
+            },
+        };
+    }
+
+    const statement = await Wallet.get_statement(wallet.id, page_number, page_size);
+
+    ctx.status = 200;
+    ctx.body = {
+        status: 'success',
+        data: {
+            statement,
+        },
+    };
+
+    await next();
+};
 
 const statement = async (ctx: ParameterizedContext, next: Next): Promise<void> => {};
 
