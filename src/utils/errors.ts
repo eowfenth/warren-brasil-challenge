0afd51643a95be6b1120572e95ea5662495c631b
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 20:38:14 -0300

    chore: update error messages

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -3,7 +3,7 @@ const errors = {
     NO_PASSWORD_LOGIN_ERROR: 'Para logar é necessário uma senha',
     WRONG_CREDENTIALS: 'Email ou senha incorretos',
     NO_PASSWORD_SIGN_UP_ERROR: 'Para cadastrar é necessário uma senha',
-    CANNOT_SIGN_UP_ERROR: 'Não foi possível finalizar o cadastrado',
+    CANNOT_SIGN_UP_ERROR: 'Não foi possível finalizar o cadastro',
     UNAUTHORIZED_ERROR: 'Não é permitido finalizar esta ação',
     CANNOT_DEPOSIT_ERROR: 'Não foi possível finalizar deposito',
     CANNOT_TRANSFER_ERROR: 'Não foi possível finalizar transferência',
