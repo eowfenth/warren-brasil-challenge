e69f25c4bde4cad6b55e0bfb1ad6b0c6cb550f08
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 15:26:49 -0300

    chore: add error message object

---
+++ src/utils/errors.ts
@@ -0,0 +1,7 @@
+const errors = {
+    NO_EMAIL_LOGIN_ERROR: 'Para logar é necessário um email',
+    NO_PASSWORD_LOGIN_ERROR: 'Para logar é necessário uma senha',
+    WRONG_CREDENTIALS: 'Email ou senha incorretos',
+};
+
+export default errors;
