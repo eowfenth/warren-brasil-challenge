621cf15464e03f3f86d3ca859da183e387e2fc5a
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 15:31:58 -0300

    feat: add controller for sign in

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,29 @@
 import { ParameterizedContext, Next } from 'koa';
+import User from '../repositories/user';
+import Errors from '../utils/errors';
+
+const sign_in = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
+    const { email = null, password = null } = ctx.request.body;
+    const { token } = ctx.state;
+
+    if (!email || !password) {
+        ctx.status = 400;
+        ctx.body = {
+            status: 'error',
+            data: { message: !email ? Errors.NO_EMAIL_LOGIN_ERROR : Errors.NO_PASSWORD_LOGIN_ERROR },
+        };
+    }
+
+    ctx.status = 200;
+    ctx.body = {
+        status: 'success',
+        data: {
+            token: token,
+        },
+    };
+
+    await next();
+};
 
 const sign_in = async (ctx: ParameterizedContext, next: Next): Promise<void> => {};
 
