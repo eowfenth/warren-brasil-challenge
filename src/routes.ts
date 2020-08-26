57698351f3447f5a4473bf0cd977d6e36fadc492
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 15:31:02 -0300

    feat: update routes to make use of auth middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import Auth from './controllers/auth';
+import AuthMiddleware from './middlewares/auth';
 import Wallet from './controllers/wallet';
 
 const router = new Router();
@@ -16,7 +17,7 @@ router.get('/wallet/:id/statement', Wallet.statement);
 router.post('/wallet/:id/payment', Wallet.payment);
 router.post('/wallet/:id/withdraw', Wallet.withdraw);
 router.post('/wallet/:id/deposit', Wallet.deposit);
-router.post('/auth/sign_in', Auth.sign_in);
+router.post('/auth/sign_in', AuthMiddleware.compare_password, Auth.sign_in);
 router.post('/auth/sign_up', Auth.sign_up);
 
 export default router;
