25788626882bab4781ec1bcb21938f1c3aa1b69a
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 18:13:20 -0300

    fix: wallet endpoints to make use of check_auth middleware

--- src/routes.ts
+++ src/routes.ts
@@ -13,10 +13,10 @@ router.get('/', async (ctx, next) => {
     await next();
 });
 
-router.get('/wallet/:id/statement', Wallet.statement);
-router.post('/wallet/:id/payment', Wallet.payment);
-router.post('/wallet/:id/withdraw', Wallet.withdraw);
-router.post('/wallet/:id/deposit', Wallet.deposit);
+router.get('/wallet/statement', AuthMiddleware.check_authorization, Wallet.statement);
+router.post('/wallet/:id/payment', AuthMiddleware.check_authorization, Wallet.payment);
+router.post('/wallet/:id/withdraw', AuthMiddleware.check_authorization, Wallet.withdraw);
+router.post('/wallet/:id/deposit', AuthMiddleware.check_authorization, Wallet.deposit);
 router.post('/auth/sign_in', AuthMiddleware.compare_password, AuthController.sign_in);
 router.post('/auth/sign_up', AuthMiddleware.encrypt_password, AuthController.sign_up);
 
