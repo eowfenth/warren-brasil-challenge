27a6b3c1474a482c7c17e5aac838838405b39658
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 23:32:47 -0300

    feat: add action and transfer wallet repo functions

--- src/repositories/wallet.ts
+++ src/repositories/wallet.ts
@@ -22,6 +22,79 @@ const get_statement = async (wallet_id: string, page_number: number, page_size: 
         .offset((page_size - 1) * page_number);
     return result;
 };
+
+const action = async (
+    wallet_id: string,
+    value: number,
+    type: 'deposit' | 'withdraw' | 'transfer_deposit' | 'transfer_withdraw',
+): Promise<Transaction | null> => {
+    const wallet: Wallet = await knex.transaction(async (trx) => {
+        const now = new Date();
+
+        const current = await trx('wallet').select().where({ deleted_at: null, id: wallet_id }).first().forUpdate();
+
+        if (!current) {
+            throw new Error(Errors.NOT_FOUND);
+        }
+
+        if (current.balance < value) {
+            throw new Error(Errors.BALANCE_NOT_ENOUGH);
+        }
+
+        const updatedWallet = await trx('wallet')
+            .where({ deleted_at: null, id: wallet_id })
+            .update({
+                amount: value,
+                updatedAt: now,
+            })
+            .returning('*');
+
+        return updatedWallet.shift();
+    });
+
+    if (wallet) {
+        const transaction = await knex.transaction(async (trx) => {
+            const result: Transaction[] = await trx('transactions')
+                .insert({
+                    id: uuid(),
+                    user_id: wallet.user_id,
+                    wallet_id: wallet.id,
+                    amount: value,
+                    type,
+                })
+                .returning('*');
+            return result.shift();
+        });
+
+        if (transaction) {
+            return transaction;
+        }
+    }
+
+    return null;
+};
+
+const transfer = async (transaction: {
+    wallet_id: string;
+    receiver_wallet_id: string;
+    value: number;
+}): Promise<{ withdraw: Transaction; deposit: Transaction } | null> => {
+    const { wallet_id, receiver_wallet_id, value } = transaction;
+    const withdraw = await action(wallet_id, value * -1, 'transfer_withdraw');
+
+    if (withdraw) {
+        const deposit = await action(receiver_wallet_id, value, 'transfer_deposit');
+
+        if (deposit) {
+            return {
+                withdraw,
+                deposit,
+            };
+        }
+    }
+
+    return null;
+};
 const create = async (user_id: string): Promise<string | null> => {
     const wallet = await knex.transaction(async (trx) => {
         const result: Wallet[] = await trx('wallets')
