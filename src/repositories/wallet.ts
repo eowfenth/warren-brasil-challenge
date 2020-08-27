40f52c966d0b6154e083228ca5b771e921cc44d2
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-27 01:33:51 -0300

    docs: update docs for wallet repo

--- src/repositories/wallet.ts
+++ src/repositories/wallet.ts
@@ -1,19 +1,32 @@
 import knex from '../integration/database';
 import { v4 as uuid } from 'uuid';
 import { Wallet, Transaction } from '../db';
-import Errors from '../utils/errors';
 
+/**
+ * Obtém informações de uma conta a partir de um id;
+ * @param wallet_id id da conta corrente
+ */
 const get_wallet = async (wallet_id: string): Promise<Wallet | null> => {
     console.log(wallet_id);
     const wallet: Wallet = await knex('wallets').select().where({ deleted_at: null, id: wallet_id }).first();
     return wallet;
 };
 
+/**
+ * Obtém informações de uma conta a partir de um id de usuário;
+ * @param user_id id do usuário dono da conta;
+ */
 const get_wallet_by_user_id = async (user_id: string): Promise<Wallet | null> => {
     const wallet: Wallet = await knex('wallets').select().where({ deleted_at: null, user_id: user_id }).first();
     return wallet;
 };
 
+/**
+ * Chamada responsável por interagir com o banco de dados e obter extratos ordenados;
+ * @param wallet_id id da conta corrente;
+ * @param page_number número da página, utilizado para paginação;
+ * @param page_size tamanho da página, utilizado para paginação;
+ */
 const get_statement = async (wallet_id: string, page_number: number, page_size: number): Promise<Transaction[]> => {
     const result: Transaction[] = await knex('transactions')
         .select()
@@ -24,6 +37,12 @@ const get_statement = async (wallet_id: string, page_number: number, page_size: 
     return result;
 };
 
+/**
+ * Chamada genérica para depositar ou sacar valores de uma conta; Assim como transferir entre contas;
+ * @param wallet_id id de uma conta;
+ * @param value valor a ser inserido, resgatado ou transferido;
+ * @param type tipo de ação (depósito, resgate ou transferência (pagamento));
+ */
 const action = async (
     wallet_id: string,
     value: number,
@@ -75,6 +94,10 @@ const action = async (
     return null;
 };
 
+/**
+ * Chamada específica para interagir com o banco de dados e efetuar uma transferência entre contas.
+ * @param transaction dados necessários para uma transferência;
+ */
 const transfer = async (transaction: {
     wallet_id: string;
     receiver_wallet_id: string;
@@ -96,6 +119,11 @@ const transfer = async (transaction: {
 
     return null;
 };
+
+/**
+ * Cria uma nova conta corrente para um usuário; 
+ * @param user_id id do usuário
+ */
 const create = async (user_id: string): Promise<string | null> => {
     const wallet = await knex.transaction(async (trx) => {
         const result: Wallet[] = await trx('wallets')
@@ -113,6 +141,5 @@ const create = async (user_id: string): Promise<string | null> => {
 
     return null;
 };
-
 
 export default { get_wallet, get_wallet_by_user_id, get_statement, transfer, create, action };
