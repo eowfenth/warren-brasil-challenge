76d560adc7a5f69d42d3c4bcb2cc80d157346b6a
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-26 23:31:24 -0300

    feat: add get_wallet and get_wallet_by_user_id wallet repo functions

--- src/repositories/wallet.ts
+++ src/repositories/wallet.ts
@@ -1,6 +1,18 @@
 import knex from '../integration/database';
 import { v4 as uuid } from 'uuid';
-import { Wallet } from '../db';
+import { Wallet, Transaction } from '../db';
+import Errors from '../utils/errors';
+
+const get_wallet = async (wallet_id: string): Promise<Wallet | null> => {
+    console.log(wallet_id);
+    const wallet: Wallet = await knex('wallets').select().where({ deleted_at: null, id: wallet_id }).first();
+    return wallet;
+};
+
+const get_wallet_by_user_id = async (user_id: string): Promise<Wallet | null> => {
+    const wallet: Wallet = await knex('wallets').select().where({ deleted_at: null, user_id: user_id }).first();
+    return wallet;
+};
 
 const create = async (user_id: string): Promise<string | null> => {
     const wallet = await knex.transaction(async (trx) => {
