e5155ff95c120788a0f828cace4632ef03e62f98
Nícolas Deçordi <eowfenth.services@gmail.com>
2020-08-27 00:10:49 -0300

    feat: add route for wallet.transfer

--- src/routes.ts
+++ src/routes.ts
@@ -14,9 +14,10 @@ router.get('/', async (ctx, next) => {
 });
 
 router.get('/wallet/statement', AuthMiddleware.check_authorization, Wallet.statement);
-router.post('/wallet/:id/payment', AuthMiddleware.check_authorization, Wallet.payment);
-router.post('/wallet/:id/withdraw', AuthMiddleware.check_authorization, Wallet.withdraw);
-router.post('/wallet/:id/deposit', AuthMiddleware.check_authorization, Wallet.deposit);
+router.post('/wallet/payment', AuthMiddleware.check_authorization, Wallet.payment);
+router.post('/wallet/transfer', AuthMiddleware.check_authorization, Wallet.transfer);
+router.post('/wallet/withdraw', AuthMiddleware.check_authorization, Wallet.withdraw);
+router.post('/wallet/deposit', AuthMiddleware.check_authorization, Wallet.deposit);
 router.post('/auth/sign_in', AuthMiddleware.compare_password, AuthController.sign_in);
 router.post('/auth/sign_up', AuthMiddleware.encrypt_password, AuthController.sign_up);
 
